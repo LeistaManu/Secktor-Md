b4b528684ade0ba0fb9b807c9592614a97ba969f
LeistaManu <148265826+LeistaManu@users.noreply.github.com>
2024-10-08 22:22:43 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 //═══════[Required Variables]════════\\
 global.owner = process.env.OWNER_NUMBER || '254718663469'
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
-global.port= process.env.PORT || 5000
+global.port= process.env.PORT || 10000
 global.email = 'sam@secktor.live'
 global.github = 'https://github.com/SamPandey001/Secktor-Md'
 global.location = 'Sultanpur IN'
